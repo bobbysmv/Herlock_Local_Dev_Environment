1e06f26c720cd37eb43fa10bab44bd6ac53f1bdd
bobby <bobby@sonicmoov.com>
2014-08-14 22:16:37 +0900

    サンプルプロジェクトの追加

---
+++ projects/sample/README.md
@@ -0,0 +1,8 @@
+# Sample Project
+
+main.jsをプロジェクトのディレクトリに作成。
+
+確認環境はこのjsファイルを起点としてアプリ起動する。
+URL↓
+
+http://[YOUR_IP]:8080/[PROJECT]/main.js
---
+++ projects/sample/main.js
@@ -0,0 +1,28 @@
+
+/**
+ * サンプルクラス
+ * @constructor
+ * 
+ * コンストラクター
+ * @param {String} value
+ */
+function Sample( value ){
+	this._value = value;
+};
+
+Sample.prototype = {
+
+    /**
+     * ログ
+     * @param {String} txt
+     */
+	log: function( txt ){ 
+		console.log( this._value + " " + txt ); 
+	}
+};
+
+
+
+var s = new Sample("test");
+s.log("aaa");
+
